ee303071eb7ab677e15e25fff6be43eca84c2fb5
Rut304 <jeff.rutman@gmail.com>
2025-12-15 14:43:52 -0500

    Add Twitter/X API utility for thread fetching
    - Created src/utils/twitter_api.py with TwitterAPI class
    - Supports single tweet fetch, full thread retrieval, search, and account monitoring
    - Uses conversation_id approach to pull entire threads from same author
    - Added convenience functions: fetch_thread(), search_prediction_markets()

---
+++ src/utils/__init__.py
@@ -0,0 +1,11 @@
+"""Utility modules for PolyBot."""
+
+from .twitter_api import TwitterAPI, fetch_thread, search_prediction_markets
+from .rate_limiter import RateLimiter
+
+__all__ = [
+    "TwitterAPI",
+    "fetch_thread", 
+    "search_prediction_markets",
+    "RateLimiter",
+]
